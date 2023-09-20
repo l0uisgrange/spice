9f328c393a8dc3e3784d8872ce35508c6fa97ccf
Louis Grange <louisgrange@ik.me>
2023-09-20 16:21:30 +0200

    Initial Commit

---
+++ Spice/SpiceApp.swift
@@ -0,0 +1,32 @@
+//
+//  SpiceApp.swift
+//  Spice
+//
+//  Created by Louis Grange on 20.09.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SpiceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
